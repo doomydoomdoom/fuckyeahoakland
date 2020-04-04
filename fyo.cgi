756610799b7738a7649824cd53fb71b9e93b8f76
Josh <josh@fuckyeahoakland.com>
2020-04-04 15:24:36 -0700

    Split fuckyeahoakland.py into two scripts

---
+++ fyo.cgi
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import json
+import getopt
+import sys
+
+from pprint import pprint
+
+config = {
+  'key':'e651fd6cb0ba73daaf9cab6b6c643257',
+  'cities':{
+    'sanfrancisco':{'lat':37.7337,'lon':-122.4467},
+    'oakland':{'lat':37.8043,'lon':-122.2711},
+    'nope':{'lat':37.7780,'lon':-122.5004}
+  }
+}
+
+temp=dict()
+phrase=str()
+comment=str()
+
+try:
+  f = open('fuckyeahoakland.json','r')
+  temp = json.load(f)
+  f.close()
+  comment=str(temp)
+except IOError:
+  e = sys.exc_info()[0]
+  phrase = "I sincerely have no idea where my data is, but technically my SLA is 'Whenever I f*cking feel like it'"
+  comment = "Where's my fucking file? %s" % e
+except:
+  e = sys.exc_info()[0]
+  phrase = "I sincerely have no idea what's going on, but technically my SLA is 'Whenever I f*cking feel like it'."
+  comment = "WTF? %s" % e
+
+try:
+  diff=round(temp['oakland']) - int(temp['sf'])
+  if diff < 1:
+    diff=round(temp['oakland']) - int(temp['nope']) 
+except KeyError:
+  diff = 0
+
+if diff >= 1:
+  phrase = "You'd be %u degrees warmer if you were in Oakland right now." % abs(diff)
+else:
+  phrase = "Either Oakland is abnormally chilly, or I broke the app. It's probably the latter."
+
+content='''
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>FUCK YEAH OAKLAND</title><meta name="description" content=""/>
+    <style type="text/css">
+        @import url("fuckyeah.css");
+    </style>
+  </head>
+  <body>
+  <h1>%s</h1>
+
+  <!--
+    %s
+  -->
+
+  <h3>
+    [<a href="http://darksky.net/poweredby/"><img src="./static/poweredby-oneline.png"></a>]
+  </h3>
+  <h4>
+    [...AND <a href="http://maps.google.com/maps/ms?ie=UTF8&hl=en&msa=0&msid=111386974984487942660.00043b9b4dc191c072885&om=0&ll=37.784554,-122.23526&spn=0.086556,0.194664&z=12">TACOS</a>]
+  </h4>
+  </body>
+</html>
+'''
+
+print(content % (phrase,comment) )
+
---
+++ refresh_darksky.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+import json
+import getopt
+import sys
+import http.client
+
+from pprint import pprint
+
+config = {
+  'key':'e651fd6cb0ba73daaf9cab6b6c643257',
+  'cities':{
+    'sf':{'lat':37.7337,'lon':-122.4467},
+    'oakland':{'lat':37.8043,'lon':-122.2711},
+    'nope':{'lat':37.7780,'lon':-122.5004}
+  }
+}
+
+temp = {
+  'oakland':0,
+  'sf':0,
+  'nope':0,
+}
+
+for city in ('sf','oakland','nope'):
+  uri = "/forecast/%s/%0.4f,%0.4f" % ( config['key'], config['cities'][city]['lat'], config['cities'][city]['lon'] )
+
+  conn = http.client.HTTPSConnection("api.darksky.net")
+  conn.request("GET",uri)
+  resp = conn.getresponse()
+  bytes=resp.read()
+  data=bytes.decode('utf-8')
+
+  try:
+    if int(resp.status) == 200:
+      try:
+        ds_json = json.loads(data)
+      except:
+        print('ERROR: Unparseable JSON')
+        pprint(data)
+        quit()
+      
+      if int(ds_json['currently']['temperature']) > 0:
+        try:
+          type(ds_json)
+          f = open('%s.json' % city,'w')
+          #f.write(data)
+          json.dump(data,f)
+          f.close()
+          temp[city]=ds_json['currently']['temperature']
+        except:
+          print("ERROR:")
+          e = sys.exc_info()[0]
+          print(e)
+          quit()
+      else:
+          print('ERROR: Bad JSON')
+          pprint(ds_json['currently'])
+          quit()
+    else:
+      print('Bad Response: %i' % resp.status())
+      pprint(data)
+      quit()
+  except:
+    print('Failed Response: %i' % resp.status)
+    pprint(resp)
+  finally:
+    conn.close()
+
+diff=round(temp['oakland']) - int(temp['sf'])
+if diff < 0:
+  #BLATANT CHEATING... cherry-pick the coldest micro-climate
+  diff=round(temp['oakland']) - int(temp['nope'])
+
+if diff < 0:
+  print("Oakland:        %f" % temp['oakland'])
+  print("Central SF:     %f" % temp['sf'])
+  print("Outer Richmond: %f" % temp['nope'])
+
+f = open('fuckyeahoakland.json','w')
+json.dump(temp,f)
+f.close()
+
