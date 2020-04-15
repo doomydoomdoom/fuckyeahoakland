0375211cb89274db45830f80f0e8b330880ab802
Josh Smith <joshsmith@kink.com>
2020-04-15 14:13:51 -0700

    Added logging for historical data, added Potrero Hill for more context on microclimates, and separated float-cleanup logic from microclimate-cherrypicking logic

--- refresh_darksky.py
+++ refresh_darksky.py
@@ -1,20 +1,41 @@
 #!/usr/bin/env python3
 import json
 import sys
+import datetime
 
+from datetime    import datetime
 from http.client import HTTPSConnection
 from pprint      import pprint
 from statistics  import mean
 
+start    = datetime.now().strftime('%Y%m%d%H%M%S')
 report   = False
 publish  = False
 forecast = {}
+
 config = {
   'key':'e651fd6cb0ba73daaf9cab6b6c643257',
   'cities':{
-    'sf':{'lat':37.7337,'lon':-122.4467},
-    'oakland':{'lat':37.8043,'lon':-122.2711},
-    'nope':{'lat':37.7780,'lon':-122.5004}
+    'sf':{
+      'name':'Central SF',
+      'lat':37.7337,
+      'lon':-122.4467,
+    },
+    'yolo':{
+      'name':'Potrero Hill',
+      'lat':37.4526,
+      'lon':-122.2359
+    },
+    'nope':{
+      'name':'Outer Richmond',
+      'lat':37.7780,
+      'lon':-122.5004
+    },
+    'oakland':{
+      'name':'The O',
+      'lat':37.8043,
+      'lon':-122.2711
+    },
   }
 }
 
@@ -57,12 +78,12 @@ for city in list(config['cities'].keys()):
         json.dump(data,f)
         f.close()
 
-        forecast[city]['temp'] = ds_json['currently']['temperature']
-        forecast[city]['feels_like'] = ds_json['currently']['apparentTemperature']
-        forecast[city]['avg'] = mean([
+        forecast[city]['temp'] = round(ds_json['currently']['temperature'],2)
+        forecast[city]['feels_like'] = round(ds_json['currently']['apparentTemperature'],2)
+        forecast[city]['avg'] = round(mean([
           ds_json['daily']['data'][0]['temperatureLow'],
-          ds_json['daily']['data'][0]['temperatureHigh']
-        ])
+          ds_json['daily']['data'][0]['temperatureHigh'],
+        ]),2)
       else:
         print('ERROR: Incorrect JSON')
         pprint(ds_json['currently'])
@@ -71,26 +92,43 @@ for city in list(config['cities'].keys()):
   finally:
     conn.close()
 
-# Cherrypick the hell out of it.
+# Cherrypick the hell out of it. ( * SNRK* )
 lies       = [int(forecast['sf']['temp']),int(forecast['sf']['feels_like']),int(forecast['sf']['avg'])]
 damn_lies  = [int(forecast['nope']['temp']),int(forecast['nope']['feels_like']),int(forecast['nope']['avg'])]
 statistics = sorted(lies + damn_lies)
-
-# *SNRK*
 zero_blind_study = sorted([int(forecast['oakland']['temp']) + 1,int(forecast['oakland']['feels_like']) + 1,int(forecast['oakland']['avg'] + 1)])
 
-forecast['min']=statistics[0]
-forecast['max']=zero_blind_study[0]
+# Display data
+forecast['min'] = statistics[0]
+forecast['max'] = zero_blind_study[0]
+
+# Diagnostic data
+forecast['start'] = start
+forecast['end']   = datetime.now().strftime('%Y%m%d%H%M%S')
+
+# Still trying to figure out the data weirdness, specifically: "Season variation, or climate/micro-climate change?".
+# Adding Potrero Hill for context, as the weirdest part is how relatively warm the Outer Richmond is/was.
+
+if publish:
+  f = open('fuckyeahhistoricaldata.json','a')
+  json.dump(forecast,f)
+  f.write("\n")
+  f.close()
+# TBD: --report should parse *this* file and actually contextualize some shit.
 
 if forecast['min'] > forecast['max']:
   report  = True
   publish = False
 
 if report:
-  pprint(forecast)
+  pprint(forecast, indent=1)
 
 if publish:
   f = open('fuckyeahoakland.json','w')
   json.dump(forecast,f)
   f.close()
+
+
+
+
 
